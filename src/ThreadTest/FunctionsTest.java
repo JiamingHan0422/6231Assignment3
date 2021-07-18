fcc900aa1be38cd296f745724ea323b84e9a890f
hanjiaming <46681023+JiiamingHan@users.noreply.github.com>
2021-07-18 13:30:35 -0300

    initial Structure

---
+++ src/ThreadTest/FunctionsTest.java
@@ -0,0 +1,98 @@
+package ThreadTest;
+
+
+import ServerFile.WebServerImpl;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.rmi.RemoteException;
+
+public class FunctionsTest {
+
+    ServerFile.WebServerImpl methodImpl;
+    File MTLFile = new File("");
+    String FilePath = MTLFile.getAbsolutePath();
+
+    /**
+     * Loading the database in DDO.
+     */
+    @Before
+    public void Before(){
+        try {
+            methodImpl = new WebServerImpl("MTL");
+            methodImpl.load("MTL");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * The test is finish.
+     */
+    @After
+    public void after(){
+
+        System.out.println("This test is OK.");
+
+    }
+
+
+    /**
+     * Test for creating the teacher record.
+     */
+    @Test
+    public void ServerTrecordTest(){
+        boolean result;
+        result = methodImpl.createTRecord(
+                "MTL11111","firstName", "lastName",
+                "Address", "Phone", "Specialization", "mtl");
+        Assert.assertEquals(result, true);
+
+        result = methodImpl.createTRecord(
+                "MTL11111","firstName", "lastName",
+                "Address", "Phone", "Specialization", "XXX");
+        Assert.assertEquals(result, false);
+
+    }
+
+    /**
+     * Test for creating the student record.
+     */
+    @Test
+    public void ServerSrecordTest(){
+        boolean result;
+        result = methodImpl.createSRecord(
+                "MTL11111","firstName", "lastName",
+                "CoursesRegistered", "active", "StatusDate");
+        Assert.assertEquals(result, true);
+
+        result = methodImpl.createSRecord(
+                "MTL11111","firstName", "lastName",
+                "CoursesRegistered", "XXX", "StatusDate");
+        Assert.assertEquals(result, false);
+
+    }
+
+    /**
+     * Test for editing record.
+     */
+    @Test
+    public void DDOServerEditrecordTest() throws RemoteException {
+        boolean result;
+        methodImpl.createSRecord(
+                "MTL11111","firstName", "lastName",
+                "CoursesRegistered", "active", "StatusDate");
+        result = methodImpl.editRecord("MTL11111", "TR10001","Location","mtl");
+        Assert.assertEquals(result, true);
+
+    }
+
+}
